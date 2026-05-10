5c95309071e6fef935945fe4051b231568c5d6e8
Manwel Nova <manwel.nova@gmail.com>
2026-05-10 15:53:04 +0200

    modif gallery et ajout filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,103 @@
-async function afficherGallery() {
+// ================ Création de la galerie dynamique ================
+
+// Variable globale pour stocker les travaux
+let works = [];
+
+function afficherWorks(data) {
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
 
+  for (const element of data) {
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+    img.src = element.imageUrl;
+    img.alt = `image de ${element.title}`;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerText = element.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  }
+}
+
+async function afficherGallery() {
   try {
     const reponse = await fetch("http://localhost:5678/api/works");
-    const data = await reponse.json();
-
-    for (const elements of data) {
-      const figure = document.createElement("figure");
-
-      const img = document.createElement("img");
-      img.src = elements.imageUrl;
-      img.alt = ` image de ${elements.title}`;
-
-      const figcaption = document.createElement("figcaption");
-      figcaption.innerText = elements.title;
-
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
-    }
+    works = await reponse.json();
+    afficherWorks(works);
   } catch (error) {
     console.error("Erreur API :", error);
     alert("Erreur réseau, impossible de charger les travaux");
   }
 }
+
 afficherGallery();
+
+// ================ Boutons filtre ================
+
+function creeBtnFiltres() {
+  const gallery = document.querySelector(".gallery");
+
+  // Créer un conteneur pour les filtres
+  const filtersContainer = document.createElement("div");
+  filtersContainer.classList.add("filters");
+
+  // Créer les boutons
+  const btnTous = document.createElement("button");
+  btnTous.innerText = "Tous";
+
+  const btnObjets = document.createElement("button");
+  btnObjets.innerText = "Objets";
+
+  const btnHotels = document.createElement("button");
+  btnHotels.innerText = "Hotels & restaurants";
+
+  const btnAppartements = document.createElement("button");
+  btnAppartements.innerText = "Appartements";
+
+  // Ajouter les boutons dans le conteneur
+  filtersContainer.appendChild(btnTous);
+  filtersContainer.appendChild(btnObjets);
+  filtersContainer.appendChild(btnAppartements);
+  filtersContainer.appendChild(btnHotels);
+
+  // Insérer le bloc filtres avant la galerie
+  gallery.before(filtersContainer);
+}
+
+creeBtnFiltres();
+
+const btnFiltres = document.querySelectorAll(".filters button");
+btnFiltres.forEach((btn) => {
+  btnFiltres[0].classList.add("active");
+  btn.addEventListener("click", () => {
+    btnFiltres.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    switch (btn.textContent) {
+      case "Tous":
+        afficherWorks(works);
+        break;
+      case "Objets":
+        const filtresObjets = works.filter(
+          (work) => work.category.name === "Objets",
+        );
+        afficherWorks(filtresObjets);
+        break;
+      case "Appartements":
+        const filtresAppartements = works.filter(
+          (work) => work.category.name === "Appartements",
+        );
+        afficherWorks(filtresAppartements);
+        break;
+      case "Hotels & restaurants":
+        const filtresHotels = works.filter(
+          (work) => work.category.name === "Hotels & restaurants",
+        );
+        afficherWorks(filtresHotels);
+        break;
+    }
+  });
+});
