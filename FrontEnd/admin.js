61828e9416836ab5d1a801474db8acc6afd1a174
Manwel Nova <manwel.nova@gmail.com>
2026-05-16 13:40:10 +0200

    ajout modale 'modifier'

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -1,22 +1,144 @@
 export function pageAdmin() {
-  // creation banniere
+  createBanner();
+  loginButton();
+  removeFilters();
+  portfolioTitle();
+}
+
+// ==================BANNIÈRE=================
+function createBanner() {
   const banner = document.createElement("div");
   banner.className = "banner";
-  banner.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Mode édition';
-  document.body.insertBefore(banner, document.body.firstChild);
+  banner.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> Mode édition`;
+  document.body.prepend(banner);
+}
 
-  // modif "login" en "Logout"
+// ==================LOGIN → LOGOUT====================
+function loginButton() {
   const btnLogin = document.querySelector(".btnLogin");
-  btnLogin.textContent = "Logout";
+  if (btnLogin) {
+    btnLogin.textContent = "Logout";
+  }
+}
 
-  //   Enlever les fitres
+// ================FILTRES====================
+function removeFilters() {
   const filters = document.querySelector(".filters");
-  filters.style.display = "none";
+  if (filters) {
+    filters.style.display = "none";
+  }
+}
 
-  //  titre mes projets et btn modif
-  const titreMesPtojets = document.querySelector("#portfolio h2");
+// =================TITRE + BOUTON MODIF=================
+function portfolioTitle() {
+  const title = document.querySelector("#portfolio h2");
+  if (!title) return;
+
+  title.style.marginTop = "150px";
+  title.style.marginBottom = "100px";
+
   const btnModif = document.createElement("span");
   btnModif.className = "btnModif";
-  btnModif.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> Modifier`;
-  titreMesPtojets.appendChild(btnModif);
+  btnModif.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> modifier`;
+
+  title.appendChild(btnModif);
+
+  btnModif.addEventListener("click", openModal);
+}
+
+// ======================MODALE====================
+function openModal() {
+  if (document.querySelector(".fondModif")) return;
+
+  const fondModif = document.createElement("div");
+  fondModif.className = "fondModif";
+
+  const fenetreModif = document.createElement("div");
+  fenetreModif.className = "fenetreModif";
+  fondModif.style.height = document.body.scrollHeight + "px";
+  fondModif.appendChild(fenetreModif);
+  document.body.prepend(fondModif);
+
+  createModalContent(fenetreModif, fondModif);
+}
+
+// ==================CONTENU MODALE======================
+function createModalContent(container, fondModif) {
+  // bouton fermer
+  const closeBtn = document.createElement("span");
+  closeBtn.className = "fermerModif";
+  closeBtn.textContent = "X";
+  container.appendChild(closeBtn);
+
+  closeBtn.addEventListener("click", () => fondModif.remove());
+
+  // fermer au clic extérieur
+  fondModif.addEventListener("click", (e) => {
+    if (e.target === fondModif) {
+      fondModif.remove();
+    }
+  });
+
+  // titre gallerie
+  const title = document.createElement("h4");
+  title.className = "titleGallery";
+  title.textContent = "Galerie photos";
+  container.appendChild(title);
+
+  // galerie
+  const galerie = document.createElement("div");
+  galerie.className = "galeriePhotos";
+  container.appendChild(galerie);
+
+  afficherGallery(galerie);
+
+  // séparateur
+  const separator = document.createElement("hr");
+  container.appendChild(separator);
+
+  // bouton ajouter
+  const btnAjouter = document.createElement("button");
+  btnAjouter.className = "btnAjouterPhoto";
+  btnAjouter.textContent = "Ajouter une photo";
+  container.appendChild(btnAjouter);
+  btnAjouter.addEventListener("click", () => {
+    console.log("ajoout");
+  });
+}
+
+// ==================API======================
+async function afficherGallery(galerie) {
+  try {
+    galerie.innerHTML = "";
+
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const works = await reponse.json();
+
+    works.forEach((work) => {
+      // wrapper image
+      const wrapper = document.createElement("div");
+      wrapper.className = "imgWrapper";
+
+      // image
+      const img = document.createElement("img");
+      img.src = work.imageUrl;
+      img.alt = work.title;
+      img.className = "galeriePhotosImg";
+
+      // boutton poubelle
+      const trash = document.createElement("i");
+      trash.className = "fa-solid fa-trash trashIcon";
+
+      trash.addEventListener("click", () => {
+        console.log("sup");
+      });
+
+      // assemblage
+      wrapper.appendChild(img);
+      wrapper.appendChild(trash);
+      galerie.appendChild(wrapper);
+    });
+  } catch (error) {
+    console.error("Erreur API :", error);
+  }
 }
